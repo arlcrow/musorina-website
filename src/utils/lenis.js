b550e6c1574065c4280078489811c3cbd8730707
Matvey Aleksandrov <matvey@arlcrow.site>
2025-02-11 12:12:56 +0300

    update

---
+++ src/utils/lenis.js
@@ -0,0 +1,26 @@
+import Lenis from "lenis";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const lenis = new Lenis();
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    const anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+    anchorLinks.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            const t = e.currentTarget;
+
+            if (t) {
+                lenis.scrollTo(t.getAttribute("href"), { offset: -132 } ?? "");
+            }
+        });
+    });
+});
